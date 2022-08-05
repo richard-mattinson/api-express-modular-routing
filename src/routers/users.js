d83ad8cf300b7c9567e77032fef9d5d8be3f5758
Richard Mattinson <richard.m@ttinson.net>
2022-08-05 16:37:13 +0100

    users standrd  complete

--- src/routers/users.js
+++ src/routers/users.js
@@ -14,15 +14,48 @@ usersRouter.get("/", (req, res) => {
 });
 
 usersRouter.get("/:id", (request, response) => {
-  const foundUser = users.find((user) => user.id === Number(request.params.id));
+    const foundUser = users.find((user) => user.id === Number(request.params.id));
+      console.log("USER GET!");
 
-  if (!foundUser) {
-    return response.status(404).json({
-      error: "I SIMPLY DO NOT KNOW WHO THAT IS",
-    });
-  }
-  response.json({
-    user: foundUser,
+    if (!foundUser) {
+      return response.status(404).json({
+        error: "BETTER LUCK NEXT TIME PLUMBER!",
+      });
+    }
+    response.json({
+      user: foundUser,
+  });
+});
+
+usersRouter.put("/", (request, response) => {
+    console.log("USER PUT!");
+    const newUser = request.body
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    response.status(201).json({ newUser })
+})
+
+usersRouter.delete("/:id", (request, response) => {
+    const foundUser = users.find((user) => user.id === Number(request.params.id));
+    console.log("USER GONE!");
+    const index = users.indexOf(foundUser)
+    const deleteUserRequest = users.splice(index, 1)
+    response.json({ user: deleteUserRequest })
+})
+
+usersRouter.put("/:id", (request, response) => {
+  const oldUser = users.find((user) => user.id === Number(request.params.id));
+  console.log("USER CHANGE!");
+  const index = users.indexOf(oldUser);
+
+  users.splice(index, 1, { ...request.body, id: oldUser.id });
+
+  const updatedUser = users.find(
+    (user) => user.id === Number(request.params.id)
+  );
+
+  response.status(201).json({
+    user: { ...updatedUser }
   });
 });
 
